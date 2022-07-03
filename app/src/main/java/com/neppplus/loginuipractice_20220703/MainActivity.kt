9c638378cabd572b4b6f331ce463eb6cfcebecc1
김민철 <kmc901206@gmail.com>
2022-07-03 12:39:05 +0900

    아이디, 비밀번호 값에 따른 분기 처리

--- app/src/main/java/com/neppplus/loginuipractice_20220703/MainActivity.kt
+++ app/src/main/java/com/neppplus/loginuipractice_20220703/MainActivity.kt
@@ -23,7 +23,19 @@ class MainActivity : AppCompatActivity() {
             val id = idEdt.text.toString() // 아이디값 변수화
             val password = passwordEdt.text.toString() //비밀번호 값 변수화
 
-
+//            1. 아이디값이 "admin", 패스워드가 "asdf"
+//            2. 아이디 값 판별
+            if (id != "admin") {
+                Toast.makeText(this, "아이디가 잘못되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+//            3. 비밀번호 값 판별
+            else if (password != "asdf") {
+                Toast.makeText(this, "비밀번호가 잘못되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+//            4. 두가지 모두 맞을때
+            else {
+                Toast.makeText(this, "관리자님 환영합니다.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
