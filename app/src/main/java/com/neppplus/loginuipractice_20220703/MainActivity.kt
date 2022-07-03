5457ea52d76ed06e17376965583b35e6c23186d6
김민철 <kmc901206@gmail.com>
2022-07-03 12:31:58 +0900

    로그인 로직 설명 시작

--- app/src/main/java/com/neppplus/loginuipractice_20220703/MainActivity.kt
+++ app/src/main/java/com/neppplus/loginuipractice_20220703/MainActivity.kt
@@ -3,6 +3,7 @@ package com.neppplus.loginuipractice_20220703
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,8 +17,14 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "회원가입버튼 클릭", Toast.LENGTH_SHORT).show()
         }
 
+//        로그인 로직 실행
         loginBtn.setOnClickListener {
-            Log.d("메인 액티비티", "로그인 버튼 클릭")
+            val id = idEdt.text.toString()
+            if (id == "") {
+
+                Toast.makeText(this, "환영합니다.", Toast.LENGTH_SHORT).show()
+            }
+
         }
     }
 }
