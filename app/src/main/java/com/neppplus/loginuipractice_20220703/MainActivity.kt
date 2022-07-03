182f795d04231f562e6b9d6497e0d1ce1133d4be
김민철 <kmc901206@gmail.com>
2022-07-03 15:41:53 +0900

    인텐트를 활용하여 화면 전환시 데이터 첨부

--- app/src/main/java/com/neppplus/loginuipractice_20220703/MainActivity.kt
+++ app/src/main/java/com/neppplus/loginuipractice_20220703/MainActivity.kt
@@ -24,31 +24,37 @@ class MainActivity : AppCompatActivity() {
 
 //        로그인 로직 실행
         loginBtn.setOnClickListener {
-//            아이디 값과 비밀번호 값을 변수화
-            val id = idEdt.text.toString() // 아이디값 변수화
-            val password = passwordEdt.text.toString() //비밀번호 값 변수화
+            val inputEmail = idEdt.text.toString()
 
-////            1. 아이디값이 "admin", 패스워드가 "asdf"
-////            2. 아이디 값 판별
-//            if (id != "admin") {
-//                Toast.makeText(this, "아이디가 잘못되었습니다.", Toast.LENGTH_SHORT).show()
-//            }
-////            3. 비밀번호 값 판별
-//            else if (password != "asdf") {
-//                Toast.makeText(this, "비밀번호가 잘못되었습니다.", Toast.LENGTH_SHORT).show()
-//            }
-////            4. 두가지 모두 맞을때
-//            else {
+            val myIntent = Intent(this, MainActivity2::class.java)
+            myIntent.putExtra("email", inputEmail)
+            startActivity(myIntent)
+
+////            아이디 값과 비밀번호 값을 변수화
+//            val id = idEdt.text.toString() // 아이디값 변수화
+//            val password = passwordEdt.text.toString() //비밀번호 값 변수화
+//
+//////            1. 아이디값이 "admin", 패스워드가 "asdf"
+//////            2. 아이디 값 판별
+////            if (id != "admin") {
+////                Toast.makeText(this, "아이디가 잘못되었습니다.", Toast.LENGTH_SHORT).show()
+////            }
+//////            3. 비밀번호 값 판별
+////            else if (password != "asdf") {
+////                Toast.makeText(this, "비밀번호가 잘못되었습니다.", Toast.LENGTH_SHORT).show()
+////            }
+//////            4. 두가지 모두 맞을때
+////            else {
+////                Toast.makeText(this, "관리자님 환영합니다.", Toast.LENGTH_SHORT).show()
+////            }
+//
+////            조건문을 한번에 물어보는 경우
+//            if (id == "admin" && password == "asdf") {
 //                Toast.makeText(this, "관리자님 환영합니다.", Toast.LENGTH_SHORT).show()
 //            }
-
-//            조건문을 한번에 물어보는 경우
-            if (id == "admin" && password == "asdf") {
-                Toast.makeText(this, "관리자님 환영합니다.", Toast.LENGTH_SHORT).show()
-            }
-            else {
-                Toast.makeText(this, "로그인에 실패하셨습니다.", Toast.LENGTH_SHORT).show()
-            }
+//            else {
+//                Toast.makeText(this, "로그인에 실패하셨습니다.", Toast.LENGTH_SHORT).show()
+//            }
         }
     }
 }
