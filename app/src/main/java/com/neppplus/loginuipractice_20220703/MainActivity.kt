bdf93c0949b7146978ea86d12df95f2ab557461f
김민철 <kmc901206@gmail.com>
2022-07-03 11:51:21 +0900

    토스트, 로그 실습

--- app/src/main/java/com/neppplus/loginuipractice_20220703/MainActivity.kt
+++ app/src/main/java/com/neppplus/loginuipractice_20220703/MainActivity.kt
@@ -2,10 +2,22 @@ package com.neppplus.loginuipractice_20220703
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        signUpBtn.setOnClickListener {
+            Toast.makeText(this, "회원가입버튼 클릭", Toast.LENGTH_SHORT).show()
+        }
+
+        loginBtn.setOnClickListener {
+            Log.d("메인 액티비티", "로그인 버튼 클릭")
+        }
     }
 }
